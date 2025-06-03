77f6217b305b0810533f3d2d1e81ade5b5fd9e55
max16code <amanzeikedi16@gmail.com>
2025-06-03 10:04:51 +0100

    first commit

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from 'next/image'
+import Button from '@/components/button/button'
 
 export default function Contact() {
   return (
-    <div className={styles.container}>contact
+    <div className={styles.container}>
+      <h1 className={styles.title}>lets keep in touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image src="/OIP.jpeg" alt="" fill={true}
+          className={styles.image}/>
+        </div>
+        <form className={styles.form}>
+          <input type='text' placeholder='name' className={styles.input} />
+          <input type='text' placeholder='email' className={styles.input} />
+          <textarea className={styles.textarea}
+           placeholder="message" id="" 
+           cols="30"
+            rows="10"
+            ></textarea>
+            <button className={styles.button}>contact</button>
+        </form>
+
+      </div>
     </div>
   )
 }
